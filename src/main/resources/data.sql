ebc2197fa2984e54fee56303a1422a9532aedcf6
JhuanCleber <jhuan.cleber@estudante.ifmt.edu.br>
2026-08-16 23:01:38 -0400

    Biblioteca

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,3 +22,23 @@ WHERE NOT EXISTS (SELECT 1 FROM jogo WHERE nome_fase = 'Formas Geométricas');
 INSERT INTO jogo (nome_fase, dificuldade, tipo_operacao, assets_url, id_administrador)
 SELECT 'Contando até 100', 'facil', 'contagem', NULL, NULL
 WHERE NOT EXISTS (SELECT 1 FROM jogo WHERE nome_fase = 'Contando até 100');
+
+INSERT INTO jogo (nome_fase, dificuldade, tipo_operacao, assets_url, id_administrador)
+SELECT 'Sequência Secreta', 'facil', 'sequencia', NULL, NULL
+WHERE NOT EXISTS (SELECT 1 FROM jogo WHERE nome_fase = 'Sequência Secreta');
+
+INSERT INTO jogo (nome_fase, dificuldade, tipo_operacao, assets_url, id_administrador)
+SELECT 'Símbolos Mágicos', 'facil', 'comparacao', NULL, NULL
+WHERE NOT EXISTS (SELECT 1 FROM jogo WHERE nome_fase = 'Símbolos Mágicos');
+
+INSERT INTO jogo (nome_fase, dificuldade, tipo_operacao, assets_url, id_administrador)
+SELECT 'Hora da Aventura', 'facil', 'horas', NULL, NULL
+WHERE NOT EXISTS (SELECT 1 FROM jogo WHERE nome_fase = 'Hora da Aventura');
+
+INSERT INTO jogo (nome_fase, dificuldade, tipo_operacao, assets_url, id_administrador)
+SELECT 'Cofrinho Mágico', 'medio', 'dinheiro', NULL, NULL
+WHERE NOT EXISTS (SELECT 1 FROM jogo WHERE nome_fase = 'Cofrinho Mágico');
+
+INSERT INTO jogo (nome_fase, dificuldade, tipo_operacao, assets_url, id_administrador)
+SELECT 'Frações Coloridas', 'medio', 'fracoes', NULL, NULL
+WHERE NOT EXISTS (SELECT 1 FROM jogo WHERE nome_fase = 'Frações Coloridas');
